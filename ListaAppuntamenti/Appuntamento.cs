40d34330c739101472515154e686f2620abb0fe1
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-21 16:06:47 +0200

    Add project files.

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    public class Appuntamento
+    {
+        public DateTime DataeOra;
+        public string Nome ;
+        public string localitaAppuntamento;
+
+    
+    public Appuntamento (DateTime DataeOra, string Nome, string localitaAppuntamento)
+    {
+        this.DataeOra = DataeOra;
+        ControllaData();    
+        this.Nome = Nome;
+        this.localitaAppuntamento = localitaAppuntamento;
+    }
+
+     public void ControllaData()
+        {
+            if (this.DataeOra < DateTime.Now)
+            {
+                throw new InvalidTimeZoneException("Data non valida");
+            }
+        }
+    
+    }
+
+
+
+}
---
+++ ListaAppuntamenti/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ListaAppuntamenti;
+
+Console.WriteLine("Quanti appuntamenti vuoi inserire?");
+
+int NrAppuntamenti = int.Parse(Console.ReadLine());
+
+List<Appuntamento> Appuntamenti = new List<Appuntamento>(NrAppuntamenti);
+
+DateTime DataAppuntamento = DateTime.Now;
+string NomeAppuntamento = "";
+string Localita = "";
+
+
+for (int i = 0; i < NrAppuntamenti; i++)
+{
+    Console.WriteLine("Data e ora: ");
+    DataAppuntamento = DateTime.Parse(Console.ReadLine());
+    Console.WriteLine("Nome: ");
+    NomeAppuntamento = Console.ReadLine();
+    Console.WriteLine("Luogo Appuntamento: ");
+    Localita = Console.ReadLine();
+    Appuntamenti.Add(new Appuntamento() { DataeOra = DataAppuntamento, Nome = NomeAppuntamento, localitaAppuntamento = Localita });
+
+}
+
+
+
